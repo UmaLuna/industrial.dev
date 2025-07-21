ce5af23dd618974bb51dca616c7fc1710799b6c0
Uma <liasuma22@gmail.com>
2025-07-21 15:07:04 +0500

    2 задание

--- Navigation/Profile/PostTableViewCell.swift
+++ Navigation/Profile/PostTableViewCell.swift
@@ -1,5 +1,6 @@
 import UIKit
 import StorageService
+import iOSIntPackage
 
 final class PostTableViewCell: UITableViewCell {
 
@@ -99,10 +100,24 @@ final class PostTableViewCell: UITableViewCell {
     }
 
     func configure(with post: Post) {
-            authorLabel.text = post.author
-            postImageView.image = UIImage(named: post.image)
-            descriptionLabel.text = post.description
-            likesLabel.text = "Likes: \(post.likes)"
-            viewsLabel.text = "Views: \(post.views)"
+        authorLabel.text = post.author
+        descriptionLabel.text = post.description
+        likesLabel.text = "Likes: \(post.likes)"
+        viewsLabel.text = "Views: \(post.views)"
+
+        if let image = UIImage(named: post.image) {
+            let imageProcessor = ImageProcessor()
+
+            imageProcessor.processImage(
+                sourceImage: image,
+                filter: .noir
+            ) { [weak self] filteredImage in
+                DispatchQueue.main.async {
+                    self?.postImageView.image = filteredImage
+                }
+            }
+        } else {
+            postImageView.image = nil
         }
     }
+}
---
+++ Podfile
@@ -0,0 +1,22 @@
+platform :ios, '13.0'
+
+target 'Navigation' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # pod 'SnapKit'
+  # Pods for Navigation
+
+end
+
+target 'StorageService' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for StorageService
+
+  target 'StorageServiceTests' do
+    # Pods for testing
+  end
+
+end
